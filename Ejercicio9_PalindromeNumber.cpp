49a4bcad3cd667db37f1db79c4b1aae4162627a9
Samuel-Q <quesadasamuel72@gmail.com>
2025-10-02 22:41:17 -0500

    Definimos un vector del tamaño que haya decidido el usuario y lo llenamos mediante un while

--- Ejercicio9_PalindromeNumber.cpp
+++ Ejercicio9_PalindromeNumber.cpp
@@ -7,4 +7,13 @@ int main(){
     int i=1;
     int j=0;
     int n;
+    cout<<"De cuantos digitos es tu numero: ";
+    cin>>n;
+    int x[n];
+    while(i<=n){
+        cout<<"ingresa tu digito "<<i<<": ";
+        cin>>x[j];
+        i++;
+        j++;
+    }
 }
